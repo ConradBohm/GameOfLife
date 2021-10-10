d3e894f3e6e5c77a7b28b742a8e659e5b3ddc79f
Conrad Bohm <conrad.bohm@hotmail.co.uk>
2021-10-10 17:36:04 +0100

    added board progression using standard game of life rules

--- main.py
+++ main.py
@@ -31,10 +31,46 @@ def render(board_state):
 
     total_print += ('-' * (len(board_state[0]) + 2))
     print(total_print)
+
+def check_neighbours(board_state, x_pos, y_pos):
+    num_of_neighbours = 0
+    for check_y in range(y_pos-1, y_pos+2):
+        if check_y < 0 or check_y > (len(board_state)-1):
+            continue
+        else:
+            for check_x in range(x_pos-1, x_pos+2):
+                if check_x < 0 or check_x > (len(board_state[0])-1):
+                    continue
+                elif check_x == x_pos and check_y == y_pos:
+                    continue
+                else:
+                    num_of_neighbours += board_state[check_y][check_x]
+
+    return num_of_neighbours
+                    
+
+def advance_board(board_state):
+    new_board_state = dead_state(len(board_state[0]),len(board_state))
+    for i in range(len(board_state)):
+        for j in range(len(board_state[0])):
+            neighbours = check_neighbours(board_state,j,i)
+            if neighbours <= 1 or neighbours > 3:
+                new_board_state[i][j] = 0
+            elif neighbours == 3:
+                new_board_state[i][j] = 1
+            elif neighbours == 2:
+                new_board_state[i][j] = board_state[i][j]
+
+    return new_board_state
     
 
 
 #print(dead_state(5,5))
-#print(random_state(5,5))
-render(random_state(20,20))
+test = random_state(20,20)
+#test = [[1,0],[1,1]]
+render(test)
+#render(random_state(20,20))
+#check_neighbours(test,1,0)
+render(advance_board(test))
+
 
