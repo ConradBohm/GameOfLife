f12aea93f11ba869e846423bcb1e5b0827e8c912
Conrad Bohm <conrad.bohm@hotmail.co.uk>
2021-10-10 12:21:58 +0100

    added basic render function for the game state

--- main.py
+++ main.py
@@ -8,20 +8,33 @@ def dead_state(width, height):
     return board_state
 
 def random_state(width, height):
-    state = dead_state(width, height)
+    board_state = dead_state(width, height)
     for i in range(height):
         for j in range(width):
             if random.random() >= 0.5:
-                state[i][j] = 0
+                board_state[i][j] = 0
             else:
-                state[i][j] = 1
+                board_state[i][j] = 1
     
-    return state
+    return board_state
+
+def render(board_state):
+    total_print = '-' * (len(board_state[0]) + 2) + '\n'
+    for row in board_state:
+        row_print = '|'
+        for value in row:
+            if value == 0:
+                row_print += ' '
+            else:
+                row_print += '#'
+        total_print += (row_print + '|\n')
+
+    total_print += ('-' * (len(board_state[0]) + 2))
+    print(total_print)
+    
 
 
-
-print(dead_state(5,5))
-
-
-print(random_state(5,5))
+#print(dead_state(5,5))
+#print(random_state(5,5))
+render(random_state(20,20))
 
