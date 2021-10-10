319282acfbab3e443bc725c7348700d6f80a9aab
Conrad Bohm <conrad.bohm@hotmail.co.uk>
2021-10-10 11:41:29 +0100

    incorperated dead_state fn into random_state fn

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 import random
 
-board_state = []
+def dead_state(width, height):
+    board_state = []
+    for i in range(height):
+        board_state.append([0]*width)
+        
+    return board_state
 
 def random_state(width, height):
+    state = dead_state(width, height)
     for i in range(height):
-        state_list = []
         for j in range(width):
-            rand = random.random()
-            if rand >= 0.5:
-                state_list.append(0)
+            if random.random() >= 0.5:
+                state[i][j] = 0
             else:
-                state_list.append(1)
+                state[i][j] = 1
+    
+    return state
 
-        board_state.append(state_list)
 
 
-random_state(5,5)
-print(board_state)
+print(dead_state(5,5))
+
+
+print(random_state(5,5))
+
